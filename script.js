720208f31db3dcaa3b975b443cd1846b438e39dc
Mike Carbonari <mikecarbonari1@gmail.com>
2021-03-08 19:55:04 -0500

    Add reset game functionality

--- script.js
+++ script.js
@@ -4,7 +4,11 @@
 const player0Element = document.querySelector('.player--0');
 const player1Element = document.querySelector('.player--1');
 
-const playerElements = document.querySelectorAll('.player');
+const player0ScoreElement = document.getElementById(`current--0`);
+const player1ScoreElement = document.getElementById('current--1');
+const player0TotalScoreElement = document.getElementById(`score--0`);
+const player1TotalScoreElement = document.getElementById('score--1');
+
 const diceElement = document.querySelector('.dice');
 
 const newGameBtn = document.querySelector('.btn--new');
@@ -12,7 +16,7 @@ const rollBtn = document.querySelector('.btn--roll');
 const holdBtn = document.querySelector('.btn--hold');
 
 // Player data
-const totalScores = [0, 0];
+let totalScores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
@@ -23,24 +27,16 @@ for (let i = 0; i <= 6; i++) {
   diceImages.push(`images/dice-${i}.png`);
 }
 
-const switchPlayer = function () {
-  currentScore = 0;
-  document.getElementById(
-    `current--${activePlayer}`
-  ).textContent = currentScore;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0Element.classList.toggle('player--active');
-  player1Element.classList.toggle('player--active');
-};
-
 /**
  * Button Handlers
  */
-rollBtn.addEventListener('click', function () {
-  // Show dice image
-  diceElement.classList.remove('hidden');
+newGameBtn.addEventListener('click', init);
 
+rollBtn.addEventListener('click', function () {
   if (playing) {
+    // Show dice image
+    diceElement.classList.remove('hidden');
+
     // Generate random dice roll (1-6)
     const diceRoll = Math.floor(Math.random() * 6) + 1;
 
@@ -68,7 +64,7 @@ holdBtn.addEventListener('click', function () {
       totalScores[activePlayer];
 
     // Check score is >= 100 if so, complete the game. Else, switch players
-    if (totalScores[activePlayer] >= 10) {
+    if (totalScores[activePlayer] >= 100) {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -84,3 +80,29 @@ holdBtn.addEventListener('click', function () {
     }
   }
 });
+
+function init() {
+  totalScores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  document.querySelector('.player--winner').classList.remove('player--winner');
+  player0Element.classList.add('player--active');
+  player1Element.classList.remove('player--active');
+
+  player0ScoreElement.textContent = 0;
+  player0TotalScoreElement.textContent = 0;
+  player1ScoreElement.textContent = 0;
+  player1TotalScoreElement.textContent = 0;
+}
+
+const switchPlayer = function () {
+  currentScore = 0;
+  document.getElementById(
+    `current--${activePlayer}`
+  ).textContent = currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+};
